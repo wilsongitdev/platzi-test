e0eb1b4e9e5e1644eb3b25f700781088851a342f
Wilson Chavesta Gonzales <wilson_13_10@hotmail.com>
2022-11-12 00:59:08 -0500

    mi primer commit

---
+++ src/main/java/com/example/platzitest/PlatziTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.platzitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/platzitest/PlatziTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platzitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
